82cf82062e9f40e67fd1cc660e3a6070413d38bd
jaynnay <yolosnow@outlook.com>
2025-02-09 04:16:23 +0800

    修改data目录，修复classpath太长问题

--- bin/windows/kafka-run-class.bat
+++ bin/windows/kafka-run-class.bat
@@ -43,42 +43,56 @@ IF ["%SCALA_BINARY_VERSION%"] EQU [""] (
 )
 
 rem Classpath addition for kafka-core dependencies
-for %%i in ("%BASE_DIR%\core\build\dependant-libs-%SCALA_VERSION%\*.jar") do (
-	call :concat "%%i"
-)
+@REM for %%i in ("%BASE_DIR%\core\build\dependant-libs-%SCALA_VERSION%\*.jar") do (
+@REM 	call :concat "%%i"
+@REM )
+
+call :concat "%BASE_DIR%\core\build\dependant-libs-%SCALA_VERSION%\*"
 
 rem Classpath addition for kafka-examples
-for %%i in ("%BASE_DIR%\examples\build\libs\kafka-examples*.jar") do (
-	call :concat "%%i"
-)
+@REM for %%i in ("%BASE_DIR%\examples\build\libs\kafka-examples*.jar") do (
+@REM 	call :concat "%%i"
+@REM )
+call :concat "%BASE_DIR%\examples\build\libs\kafka-examples\*"
 
 rem Classpath addition for kafka-clients
-for %%i in ("%BASE_DIR%\clients\build\libs\kafka-clients*.jar") do (
-	call :concat "%%i"
-)
+@REM for %%i in ("%BASE_DIR%\clients\build\libs\kafka-clients*.jar") do (
+@REM 	call :concat "%%i"
+@REM )
+call :concat "%BASE_DIR%\clients\build\libs\kafka-clients\*"
 
 rem Classpath addition for kafka-streams
-for %%i in ("%BASE_DIR%\streams\build\libs\kafka-streams*.jar") do (
-	call :concat "%%i"
-)
+@REM for %%i in ("%BASE_DIR%\streams\build\libs\kafka-streams*.jar") do (
+@REM 	call :concat "%%i"
+@REM )
+
+call :concat "%BASE_DIR%\streams\build\libs\kafka-streams\*"
 
 rem Classpath addition for kafka-streams-examples
-for %%i in ("%BASE_DIR%\streams\examples\build\libs\kafka-streams-examples*.jar") do (
-	call :concat "%%i"
-)
+@REM for %%i in ("%BASE_DIR%\streams\examples\build\libs\kafka-streams-examples*.jar") do (
+@REM 	call :concat "%%i"
+@REM )
 
-for %%i in ("%BASE_DIR%\streams\build\dependant-libs-%SCALA_VERSION%\rocksdb*.jar") do (
-	call :concat "%%i"
-)
+call :concat "%BASE_DIR%\streams\examples\build\libs\kafka-streams-examples\*"
+
+@REM for %%i in ("%BASE_DIR%\streams\build\dependant-libs-%SCALA_VERSION%\rocksdb*.jar") do (
+@REM 	call :concat "%%i"
+@REM )
+
+call :concat "%BASE_DIR%\streams\build\dependant-libs-%SCALA_VERSION%\rocksdb\*"
 
 rem Classpath addition for kafka tools
-for %%i in ("%BASE_DIR%\tools\build\libs\kafka-tools*.jar") do (
-	call :concat "%%i"
-)
+@REM for %%i in ("%BASE_DIR%\tools\build\libs\kafka-tools*.jar") do (
+@REM 	call :concat "%%i"
+@REM )
 
-for %%i in ("%BASE_DIR%\tools\build\dependant-libs-%SCALA_VERSION%\*.jar") do (
-	call :concat "%%i"
-)
+call :concat "%BASE_DIR%\tools\build\libs\kafka-tools\*"
+
+@REM for %%i in ("%BASE_DIR%\tools\build\dependant-libs-%SCALA_VERSION%\*.jar") do (
+@REM 	call :concat "%%i"
+@REM )
+
+call :concat "%BASE_DIR%\tools\build\dependant-libs-%SCALA_VERSION%\*"
 
 for %%p in (api runtime file json tools) do (
 	for %%i in ("%BASE_DIR%\connect\%%p\build\libs\connect-%%p*.jar") do (
@@ -90,9 +104,12 @@ for %%p in (api runtime file json tools) do (
 )
 
 rem Classpath addition for release
-for %%i in ("%BASE_DIR%\libs\*") do (
-	call :concat "%%i"
-)
+@REM for %%i in ("%BASE_DIR%\libs\*") do (
+@REM 	call :concat "%%i"
+@REM )
+
+call :concat "%BASE_DIR%\libs\*;"
+
 
 rem Classpath addition for core
 for %%i in ("%BASE_DIR%\core\build\libs\kafka_%SCALA_BINARY_VERSION%*.jar") do (
@@ -178,7 +195,7 @@ IF not defined CLASSPATH (
 
 set COMMAND=%JAVA% %KAFKA_HEAP_OPTS% %KAFKA_JVM_PERFORMANCE_OPTS% %KAFKA_JMX_OPTS% %KAFKA_LOG4J_OPTS% -cp "%CLASSPATH%" %KAFKA_OPTS% %*
 rem echo.
-rem echo %COMMAND%
+echo %COMMAND% > args.txt
 rem echo.
 %COMMAND%
 
